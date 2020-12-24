b0c835a78539cf39d9fe503e3e9223bbb2ebceee
Alexander Alekseev <aapsoftware@gmail.com>
2020-12-24 09:49:51 +0300

    .idea

--- error_reporting.php
+++ error_reporting.php
@@ -4,11 +4,11 @@
 </head>
 <body>
 <?php
-
+strict_types(1);
 //error_reporting(-1);
 error_reporting(E_ALL);
 //ini_set('display_startup_errors',true);
-in_set('display_errors',true);
+ini_set('display_errors',true);
 ini_set('error_reporting',E_ALL);
 echo "<p>Begin";
 $s=file_get_contents("not_existing_file.txt");
