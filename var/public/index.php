9b937d5f0edf2e46cc204ace6e7b177bd0ff6c35
Alexander Alekseev <aapsoftware@gmail.com>
2021-01-14 10:33:50 +0300

    Try fix PHP files

--- var/public/index.php
+++ var/public/index.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * About file
+ *
+ * @author    Alexander Alekseev <alekseev_ap@mail.ru>
+ * @copyright 2021 Noname
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * @cathegory cathegory
+ * @package package
+ * @link http://www.nix.ru
+ *
+ */
+
 declare(strict_types=1);
 
 namespace Work;
@@ -8,7 +20,6 @@ require '../../bootstrap.php';
 
 $router = new \Work\Routers\Router();
 $controllerName = $router->getController();
-echo $controllerName;
 if ($controllerName != '') {
     $controller = new $controllerName();
     $controller->process();
